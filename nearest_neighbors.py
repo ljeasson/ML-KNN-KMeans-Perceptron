9b088f6fc30fd35c7eac399ccae9d1c905ea5ddd
Lee Easson <ljeasson@gmail.com>
2019-10-04 17:39:25 -0700

    Add perceptron_train() function to perceptron.py

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -1,7 +1,27 @@
 import numpy as np
 
-def KNN_test(X_train,Y_train,X_test,Y_test,K):
+def KNN_test(X_train, Y_train, X_test, Y_test, K):
     return 0
 
 def choose_K(X_train,Y_train,X_val,Y_val):
     return 0
+
+X_train = np.array( [[1,1], [2,1], [0,10], [10,10], [5,5], [3,10], [9,4], [6,2], [2,2], [8,7]] )
+Y_train = np.array( [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1] )
+
+acc_K1 = KNN_test(X_train, Y_train, X_train, Y_train, 1)
+acc_K2 = KNN_test(X_train, Y_train, X_train, Y_train, 3)
+acc_K3 = KNN_test(X_train, Y_train, X_train, Y_train, 5)
+
+'''
+test1 = (1,1,1)
+test2 = (2,1,-1)
+test3 = [0,10,1]
+test4 = [10,10,−1]
+test5 = [5,5,1]
+test6 = [3,10,−1]
+test7 = [9,4,1]
+test8 = [6,2,−1]
+test9 = [2,2,1]
+test10 = [8,7,−1]
+'''
--- perceptron.py
+++ perceptron.py
@@ -1,12 +1,60 @@
 import numpy as np
 
+def calculate_activation(features, weights, bias):
+    a = 0
+    for i in range(len(features)):
+        a += features[i] * weights[i]
+    a += bias
+    return a
+
+def update_weight(weight, x, y):
+    return weight + (x * y)
+
+def update_bias(bias, y):
+    return bias + y
+
 def perceptron_train(X,Y):
-    return 0
+    num_samples = X.shape[0]
+    num_features = X.shape[1]
+    print("Num Samples:  ",num_samples)
+    print("Num Features: ",num_features)
+
+    weights = np.ones(num_features, dtype=int)
+    print("Weights: ",weights)
+    bias = 0
+    print("Bias: ",bias,"\n")
+
+    for sample in range(num_samples):
+        activation = calculate_activation(X[sample], weights, bias)
+        print("Activation: ",activation)
+        current_label = Y[sample]
+        print("Current Label: ",current_label)
+        
+        if activation * current_label <= 0:
+            # Modify with better test data
+            for w in range(len(weights)):
+                print("Old Weight: ",weights[w])
+                weights[w] = update_weight(weights[w], X[sample][w], Y[sample])
+                print("New Weight: ",weights[w])
+
+            print("Old Bias: ",bias)    
+            bias = update_bias(bias, Y[sample])
+            print("New Bias: ",bias) 
+        
+        print()
+
+    print(weights," ",bias)
+    return (weights, bias)
 
 def perceptron_test(X_test, Y_test, w, b):
     return 0
 
+
 X = np.array( [[0,1], [1,0], [5,4], [1,1], [3,3], [2,4], [1,6]] )
 Y = np.array( [[1], [1], [0], [1], [0], [0], [0]] )
+
+#X = np.array( [[-2,1], [1,1], [1.5,-0.5], [-2,-1], [-1,-1.5], [2,2]] )
+#Y = np.array( [[1], [1], [1], [-1], [-1], [-1]] )
+
 W = perceptron_train(X,Y)
-#test_acc = perceptron_test(X,Y,W[0],W[1])
+test_acc = perceptron_test(X,Y,W[0],W[1])
