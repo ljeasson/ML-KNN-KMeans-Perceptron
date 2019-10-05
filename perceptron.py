73c1c5550f9af0793462c51dac63bdd87ff2e8cd
Lee Easson <ljeasson@gmail.com>
2019-10-04 19:24:42 -0700

    Remove print statements and add comments

--- perceptron.py
+++ perceptron.py
@@ -14,35 +14,29 @@ def update_bias(bias, y):
     return bias + y
 
 def perceptron_train(X,Y):
+    # Number of samples and features
     num_samples = X.shape[0]
     num_features = X.shape[1]
-    print("Num Samples:  ",num_samples)
-    print("Num Features: ",num_features)
-
+    
+    # Initialize weights to 1
+    # and bias to 0
     weights = np.ones(num_features, dtype=int)
-    print("Weights: ",weights)
     bias = 0
-    print("Bias: ",bias,"\n")
-
+    
+    # Calculate weights and bias
     for sample in range(num_samples):
+        # Calculate activation and set current label
         activation = calculate_activation(X[sample], weights, bias)
-        print("Activation: ",activation)
         current_label = Y[sample]
-        print("Current Label: ",current_label)
         
+        # If y * a <= 0, update weights and bias
         if activation * current_label <= 0:
-            # Modify with better test data
             for w in range(len(weights)):
-                print("Old Weight: ",weights[w])
                 weights[w] = update_weight(weights[w], X[sample][w], Y[sample])
-                print("New Weight: ",weights[w])
 
-            print("Old Bias: ",bias)    
             bias = update_bias(bias, Y[sample])
-            print("New Bias: ",bias) 
-        
-        print()
 
+    # Output weights and bias
     print(weights," ",bias)
     return (weights, bias)
 
@@ -50,11 +44,11 @@ def perceptron_test(X_test, Y_test, w, b):
     return 0
 
 
-X = np.array( [[0,1], [1,0], [5,4], [1,1], [3,3], [2,4], [1,6]] )
-Y = np.array( [[1], [1], [0], [1], [0], [0], [0]] )
+#X = np.array( [[0,1], [1,0], [5,4], [1,1], [3,3], [2,4], [1,6]] )
+#Y = np.array( [[1], [1], [0], [1], [0], [0], [0]] )
 
-#X = np.array( [[-2,1], [1,1], [1.5,-0.5], [-2,-1], [-1,-1.5], [2,2]] )
-#Y = np.array( [[1], [1], [1], [-1], [-1], [-1]] )
+X = np.array( [[-2,1], [1,1], [1.5,-0.5], [-2,-1], [-1,-1.5], [2,2]] )
+Y = np.array( [[1], [1], [1], [-1], [-1], [-1]] )
 
 W = perceptron_train(X,Y)
 test_acc = perceptron_test(X,Y,W[0],W[1])
