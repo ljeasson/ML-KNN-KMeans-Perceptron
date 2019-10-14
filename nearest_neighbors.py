41da2634a0f9ddf413829b6ddfa8d5bfe79186f5
Lee Easson <ljeasson@go.olemiss.edu>
2019-10-13 19:46:27 -0700

    Add perceptron_test() function

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -1,14 +1,48 @@
 import numpy as np
+import math
+
+def euclidean_distance(data1, data2, length):
+    distance = 0
+    for i in range(length):
+        distance += pow((data1[i] - data2[i]), 2)
+    return math.sqrt(distance)
 
 def KNN_test(X_train, Y_train, X_test, Y_test, K):
+    # Initialize list of distances
+    distances = []
+    # Get length of training data
+    length = X_train.shape[0]
+    
+    # Iterate through points in training data (TODO)
+    for i in range(length):
+        dist = euclidean_distance(X_train[i], X_test[i], length)
+        distances.append(dist)
+
+    # Sort distances
+    distances.sort()
+    print(distances)
+
+    # Find K nearest neighbors
+    neighbors = []
+    for i in range(K):
+        neighbors.append(distances[i])
+
+    print(neighbors)   
+
     return 0
 
 def choose_K(X_train,Y_train,X_val,Y_val):
     return 0
 
-X_train = np.array( [[1,1], [2,1], [0,10], [10,10], [5,5], [3,10], [9,4], [6,2], [2,2], [8,7]] )
-Y_train = np.array( [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1] )
 
+X_train = np.array( [[1,1], [2,1], [0,10], [10,10], [5,5], [3,10], [9,4], [6,2], [2,2], [8,7]] )
+Y_train = np.array( [[1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1]] )
+acc_K1 = KNN_test(X_train, Y_train, X_train, Y_train, 1)
+acc_K2 = KNN_test(X_train, Y_train, X_train, Y_train, 3)
+acc_K3 = KNN_test(X_train, Y_train, X_train, Y_train, 5)
+
+X_train = np.array( [[1,5], [2,6], [2,7], [3,7], [3,8], [4,8], [5,1], [5,9], [6,2], [7,2], [7,3], [8,3], [8,4], [9,5]] )
+Y_train = np.array( [[-1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [-1], [1], [1]] )
 acc_K1 = KNN_test(X_train, Y_train, X_train, Y_train, 1)
 acc_K2 = KNN_test(X_train, Y_train, X_train, Y_train, 3)
 acc_K3 = KNN_test(X_train, Y_train, X_train, Y_train, 5)
--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def calculate_activation(features, weights, bias):
     a = 0
@@ -37,18 +38,57 @@ def perceptron_train(X,Y):
             bias = update_bias(bias, Y[sample])
 
     # Output weights and bias
-    print(weights," ",bias)
     return (weights, bias)
 
+
 def perceptron_test(X_test, Y_test, w, b):
-    return 0
+    score = 0
+    length = len(Y_test)
+
+    for i in range(len(Y_test)):
+        if w[0]*X_test[i][0] + w[1]*X_test[i][1] + b[0] == Y_test[i][0]:
+            score += 1
+
+    accuracy = score/length
+    print(accuracy)
+    return accuracy
 
 
-#X = np.array( [[0,1], [1,0], [5,4], [1,1], [3,3], [2,4], [1,6]] )
-#Y = np.array( [[1], [1], [0], [1], [0], [0], [0]] )
-
-X = np.array( [[-2,1], [1,1], [1.5,-0.5], [-2,-1], [-1,-1.5], [2,2]] )
-Y = np.array( [[1], [1], [1], [-1], [-1], [-1]] )
-
+# Percepton Test Data 1
+X = np.array( [[0,1], [1,0], [5,4], [1,1], [3,3], [2,4], [1,6]] )
+Y = np.array( [[1], [1], [0], [1], [0], [0], [0]] )
 W = perceptron_train(X,Y)
 test_acc = perceptron_test(X,Y,W[0],W[1])
+
+# Percepton Test Data 2
+X = np.array( [[0,1], [1,0], [5,4], [1,1], [3,3], [2,4], [1,6]])
+Y = np.array( [[1], [1], [-1], [1], [-1], [-1], [-1]] )
+W = perceptron_train(X, Y)
+test_acc = perceptron_test(X, Y, W[0], W[1])
+
+
+# Perceptron Test Data - Writeup
+X = np.array( [[-2,1], [1,1], [1.5,-0.5], [-2,-1], [-1,-1.5], [2,-2]] )
+Y = np.array( [[1], [1], [1], [-1], [-1], [-1]] )
+W = perceptron_train(X,Y)
+test_acc = perceptron_test(X,Y,W[0],W[1])
+
+'''
+# Graph weight vector
+w1 = W[0][0]
+w2 = W[0][1]
+b  = W[1][0]
+print("W1:",w1," W2:",w2," b:",b)
+
+slope = -(b/w2)/(b/w1)  
+intercept = -b/w2
+
+x = np.linspace(-3, 3, 10)
+y = slope*x + intercept
+
+plt.title('Graph of Resulting Weight Vector')
+plt.plot(x, y, '-r', label='')
+
+plt.show(block=False)
+input('press <ENTER> to continue')
+'''
